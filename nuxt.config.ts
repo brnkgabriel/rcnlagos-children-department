457e3a028ceb7e62348606445c42b561c47939b2
brnkgabriel <brnkgabriel@gmail.com>
2023-05-08 11:28:23 +0100

    restored original pwa module

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,9 +1,10 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   srcDir: "src",
-  modules: ['@vite-pwa/nuxt'], 
+  modules: [
+    '@kevinmarrec/nuxt-pwa'
+  ],
   pwa: {
-    registerType: 'autoUpdate',
     manifest: {
       name: 'RCN Lagos - Children Department',
       short_name: 'RCN Lagos - Children Department',
@@ -12,39 +13,28 @@ export default defineNuxtConfig({
         {
           "src": "/icon-192x192.png",
           "sizes": "192x192",
-          "type": "image/png"
+          "type": "image/png",
+          "purpose": "maskable"
         },
         {
           "src": "/icon-256x256.png",
           "sizes": "256x256",
-          "type": "image/png"
+          "type": "image/png",
+          "purpose": "maskable"
         },
         {
           "src": "/icon-384x384.png",
           "sizes": "384x384",
-          "type": "image/png"
+          "type": "image/png",
+          "purpose": "maskable"
         },
         {
           "src": "/icon-512x512.png",
           "sizes": "512x512",
           "type": "image/png",
-          "purpose": "any maskable"
+          "purpose": "maskable"
         }
       ]
-    },
-    // workbox: {
-    //   navigateFallback: '/',
-    //   globPatterns: ['src/**/*.{js,css,html,png,svg,ico}'],
-    // },
-    client: {
-      installPrompt: true,
-      // you don't need to include this: only for testing purposes
-      // if enabling periodic sync for update use 1 hour or so (periodicSyncForUpdates: 3600)
-      periodicSyncForUpdates: 20,
-    },
-    devOptions: {
-      enabled: false,
-      type: 'module',
     },
   },
   css: ['~/assets/css/main.css'],
